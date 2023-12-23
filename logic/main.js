c834dd91ff4e74cbfb909f5622a9cb819f24f5bb
HPatto <hpatto97@gmail.com>
2023-12-23 10:17:00 -0300

    Add userInput functions

--- logic/main.js
+++ logic/main.js
@@ -1,4 +1,7 @@
 /*
 A JavaScript file collecting the primary functions of the calculator.
 Imports make reference to classes and their methods within other files.
+
+N.B. All inputs are handled as strings until actual math is required.
 */
+
---
+++ logic/userInput.js
@@ -0,0 +1,19 @@
+/*
+The functions corresponding to each button available to the user.
+
+A click is NOT guaranteed to execute the function.
+A click is NOt guaranteed to execute only that function.
+However, that logic is handled in ####.
+
+Each button shall only have one function associated with it.
+*/
+
+// Determine type of button pressed by the user.
+function determineButtonPressed(event) {
+    // Can we pass in events like this?
+}
+
+// Get the numeral of the button clicked and pass it on.
+function getNumeral(buttonElement) {
+    
+}
